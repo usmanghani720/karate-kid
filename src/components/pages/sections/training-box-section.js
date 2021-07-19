c18c4e3492a09a5ad73b30637ed9900805809de6
codezcracker <codez.cracker@gmail.com>
2021-07-19 17:22:49 +0400

    Popup

--- src/components/pages/sections/training-box-section.js
+++ src/components/pages/sections/training-box-section.js
@@ -6,6 +6,7 @@ import trainingIcon3 from "../../../images/Teens & Adults@2x.png";
 import trainingIcon4 from "../../../images/Grappling@2x.png";
 import trainingIcon5 from "../../../images/Extreme@2x.png";
 import trainingIcon6 from "../../../images/Weapon@2x.png";
+import { IoMdCloseCircleOutline } from 'react-icons/io';
 
 const trainingList = [
 	{
@@ -34,10 +35,28 @@ const trainingList = [
 	}
 ];
 
+function hide(event){
+	event.target.parentElement.parentElement.classList.add("active");
+//	this.event.parentNode.classList.add("active");
+}
+
 class TrainingBoxSection extends React.Component {
+
+
 	render() {
 		return (
 			<section id="training-box-section" className="training-box-section">
+				<div className="Popup">
+					<a onClick={hide} className="popup-close-button"><IoMdCloseCircleOutline /></a>
+					<div className="popup-image-holder">
+						<img src={trainingIcon1} />
+					</div>
+					<div className="popup-content-holder">
+						<h2>4 - 6 Year Old</h2>
+						<p>Evolution Martial Arts' Kinderkicker program is specially designed for children 4 through 6 years old.</p>
+						<p>Focus, concentration and coordination are taught in a positive learning environment to reinforce the foundations for building life long success skills in addition to providing basic self defense skills to keep your young child safe.</p>
+					</div>
+				</div>
 				<ul>
 					{
 						trainingList.map(trainingItem => (
